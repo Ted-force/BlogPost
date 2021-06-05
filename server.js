cf4c7a8c20281653a5904bcaec132c1f7865e6e8
Piyush Rangari <81739199+Ted-force@users.noreply.github.com>
2021-06-05 14:46:16 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var _ = require('lodash');
+
+let posts = [];
+
+app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+PORT = 3000 || process.env.PORT;
+
+app.get("/",(req,res) => {
+    res.render('index',{posts});
+})
+
+app.get("/contact",(req,res) => {
+    res.render('contact');
+});
+
+app.get("/about",(req,res) => {
+    res.render("about")
+})
+
+app.get("/compose",(req,res) => {
+    res.render('compose')
+})
+
+app.post("/compose",(req,res) => {
+    const postObj = {
+        title: req.body.postTitle,
+        content: req.body.postBody
+    }
+
+    posts.push(postObj);
+
+    res.redirect("/");
+})
+
+app.get("/post/:postName", (req,res) => {
+
+    const reqPost = _.lowerCase(req.params.postName);
+    posts.forEach(function(post){
+        const AvaPost = _.lowerCase(post.title);
+        if(reqPost === AvaPost) {
+          res.render("post",{
+              Title: post.title,
+              Content: post.content
+          })
+        }
+    })
+    
+})
+
+app.listen(PORT,function() {
+    console.log("server live at port:" + PORT);
+})
+
+
