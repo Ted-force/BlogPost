7eeda7f76d5f8aa1fddba60c7f4dd45872a0d509
Piyush Rangari <81739199+Ted-force@users.noreply.github.com>
2021-06-05 14:58:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let posts = [];
 app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static("Public"));
 
 PORT = 3000 || process.env.PORT;
 
